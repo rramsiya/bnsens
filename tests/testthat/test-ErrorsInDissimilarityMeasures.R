cc10b3e3719b7c4d4e15a658ab34cf82f442093e
Ramsiya Ramanathan <r.ramsiya97@gmail.com>
2019-07-26 12:43:42 +0200

    Tests for errors/warnings in dissimilarity measures

---
+++ tests/testthat/test-ErrorsInDissimilarityMeasures.R
@@ -0,0 +1,18 @@
+context("Errors in dissimilarity measures (CD_distance and KL)")
+library(bnsens)
+
+data<-data("travel")
+
+test_that("error/warning with order preserving scheme", {
+  expect_error(CD_distance(bnfit = travel,node = "T",value_node = "car",value_parents = c("emp","big"),new_value = "all",covariation = "orderp"))
+  expect_warning(CD_distance(bnfit = travel,node = "T",value_node = "car",value_parents = c("emp","big"),new_value = "all",covariation = "all"))
+  expect_error(KL(bnfit = travel,node = "T",value_node = "car",value_parents = c("emp","big"),new_value = "all",covariation = "orderp"))
+  expect_warning(KL(bnfit = travel,node = "T",value_node = "car",value_parents = c("emp","big"),new_value = "all",covariation = "all"))
+  })
+
+test_that("error/warnings for plots margins or existence", {
+  expect_warning(expect_error(CD_distance(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = seq(0.50,0.95,0.05),covariation = "orderp")))
+  expect_warning(CD_distance(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = seq(0.55,0.95,0.05),covariation = "orderp"),"The plot won't be showed since all the values are not possible")
+  expect_warning(expect_error(KL(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = seq(0.50,0.95,0.05),covariation = "orderp")))
+  expect_warning(KL(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = seq(0.55,0.95,0.05),covariation = "orderp"),"The plot won't be showed since all the values are not possible")
+  })
