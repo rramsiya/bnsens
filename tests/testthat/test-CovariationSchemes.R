1a873fbcc6283b5cdde361b7e6f71f39d71198ac
Ramsiya Ramanathan <r.ramsiya97@gmail.com>
2019-08-07 17:23:15 +0200

    test output for covariation schemes

--- tests/testthat/test-CovariationSchemes.R
+++ tests/testthat/test-CovariationSchemes.R
@@ -10,6 +10,13 @@ test_that("Correct class",{
   expect_true(is.na(orderp_covar(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = 0.65)))
   })
 
+test_that("Correct output CD_distance",{
+  expect_equal(uniform_covar(bnfit = travel,node = "T",value_node = "car",value_parents = c("emp","big"),new_value = 0.65)[["T"]][["prob"]][t(c("car","emp","big"))],0.65)
+  expect_equal(proportional_covar(bnfit = travel,node = "T",value_node = "car",value_parents = c("emp","big"),new_value = 0.65)[["T"]][["prob"]][t(c("car","emp","big"))],0.65)
+  expect_equal(orderp_covar(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = 0.3)[["T"]][["prob"]][t(c("train","emp","big"))],0.3)
+  expect_true(is.na(orderp_covar(bnfit = travel,node = "T",value_node = "train",value_parents = c("emp","big"),new_value = 0.65)))
+})
+
 test_that("error when invalid node name", {
   expect_error(uniform_covar(bnfit = travel,node = "t",value_node = "car",value_parents = c("emp","big"),new_value = 0.65),"Invalid input for node")
   expect_error(proportional_covar(bnfit = travel,node = "t",value_node = "car",value_parents = c("emp","big"),new_value = 0.65),"Invalid input for node")
