9bbb38d7ec7724fe468cf456f690ff3b7d672ad9
Ramsiya Ramanathan <r.ramsiya97@gmail.com>
2019-11-21 10:38:30 +0100

    bnsens_example
    (da rivedere)

--- R/bnsens_example.R
+++ R/bnsens_example.R
@@ -1,12 +1,12 @@
 #' Example of Bayesian network
 #'
-#'\code{example} is a bn.fit object for a simple Bayesian network involving three variables.
+#'\code{bnsens_example} is a bn.fit object for a simple Bayesian network involving three variables.
 #'
 #' @docType data
 #'
-#' @usage data(example)
+#' @usage data(bnsens_example)
 #'
-#' @format The Bayesian network \code{example} comprehends the following nodes:
+#' @format The Bayesian network \code{bnsens_example} comprehends the following nodes:
 #' \itemize{
 #'   \item \bold{y1}: three-level factor with levels \emph{1}, \emph{2}, \emph{3}.
 #'   \item \bold{y2}: three-level factor with levels \emph{1}, \emph{2}, \emph{3}.
